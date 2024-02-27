b7994ed2a746ab4240247295abc9e5e932eb80a9
akhileshsonwani <a.sonwani1@gmail.com>
2024-02-27 19:26:11 +0530

    some updates in form validation

--- src/assets/projct.js
+++ src/assets/projct.js
@@ -1,6 +1,7 @@
 
 function validation()
 {
+    var flag=false;
 var yourName =document.getElementById("yourName").value;
 var yourEmail =document.getElementById("yourEmail").value;
 var subject =document.getElementById("subject").value;
@@ -9,55 +10,55 @@ var message =document.getElementById("message").value;
 
 if(yourName == "")
 {
-document.getElementById("yourNamee").innerHTML="**please write yourName in the box**"; 
+document.getElementById("yourNamee").innerHTML="**please write yourName**"; 
+flag=false;
 }
 
 
 if(yourEmail == "")
 {
-document.getElementById("yourEmaill").innerHTML="**please write yourEmail in the box**";
+document.getElementById("yourEmaill").innerHTML="**please write yourEmail**";
+flag=false;
 }
 
 
 if(subject == "")
 {
-document.getElementById("subjectt").innerHTML="**please write subject in the box**";
+document.getElementById("subjectt").innerHTML="**please write subject**";
+flag=false;
 }
-
 
 
 if(message == "")
 {
-document.getElementById("messagee").innerHTML="**please write message in the box**";
+document.getElementById("messagee").innerHTML="**please write message**";
+flag=false;
 }
-
 
 
 
 
 if(yourName=="")
 {
-    
-   
+    flag=false;
 }
 else if(yourEmail=="")
 {
-   
+    flag=false;
 }
 else if(subject=="")
 {
-    
+    flag=false;
 }
 else if(message=="")
 {
-  
+    flag=false;
 }
 else{
-   
 
     document.getElementById("yourForm").reset();
-    
+    flag=true;
 }
-
+return flag;
 }
 
