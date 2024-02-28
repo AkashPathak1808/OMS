6f6910f6d7d53d35ee2fd961bdaf564a29927a03
akhileshsonwani <a.sonwani1@gmail.com>
2024-02-28 14:50:51 +0530

    update in form validation

--- src/assets/projct.js
+++ src/assets/projct.js
@@ -1,64 +1,62 @@
 
-function validation()
-{
-    var flag=false;
-var yourName =document.getElementById("yourName").value;
-var yourEmail =document.getElementById("yourEmail").value;
-var subject =document.getElementById("subject").value;
-var message =document.getElementById("message").value;
+
+function validation() {
+    var flag = true;
+    var yourName = document.getElementById("yourName").value;
+    var yourEmail = document.getElementById("yourEmail").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    if (yourName == "") {
+        document.getElementById("yourNamee").innerHTML = "* Please write your-Name";
+        flag = false;
+    } 
+    document.getElementById("yourName").addEventListener("input", function () {
+        document.getElementById("yourNamee").innerHTML = "";
+    });
+    
+    
+
+    if (yourEmail == "") {
+        document.getElementById("yourEmaill").innerHTML = "* Please write your-Email";
+        flag = false;
+    } 
+
+    document.getElementById("yourEmail").addEventListener("input", function () {
+        document.getElementById("yourEmaill").innerHTML = "";
+    });
 
 
-if(yourName == "")
-{
-document.getElementById("yourNamee").innerHTML="**please write yourName**"; 
-flag=false;
+    if (subject == "") {
+        document.getElementById("subjectt").innerHTML = "* Please write subject";
+        flag = false;
+    }
+    document.getElementById("subject").addEventListener("input", function () {
+        document.getElementById("subjectt").innerHTML = "";
+    });
+
+
+
+    if (message == "") {
+        document.getElementById("messagee").innerHTML = "* Please write message";
+        flag = false;
+    }
+
+
+    document.getElementById("message").addEventListener("input", function () {
+        document.getElementById("messagee").innerHTML = "";
+    });
+    
+
+    if (flag) {
+        
+        document.getElementById("yourForm").reset();
+    }
+
+    return flag;
 }
 
 
-if(yourEmail == "")
-{
-document.getElementById("yourEmaill").innerHTML="**please write yourEmail**";
-flag=false;
-}
 
 
-if(subject == "")
-{
-document.getElementById("subjectt").innerHTML="**please write subject**";
-flag=false;
-}
-
-
-if(message == "")
-{
-document.getElementById("messagee").innerHTML="**please write message**";
-flag=false;
-}
-
-
-
-
-if(yourName=="")
-{
-    flag=false;
-}
-else if(yourEmail=="")
-{
-    flag=false;
-}
-else if(subject=="")
-{
-    flag=false;
-}
-else if(message=="")
-{
-    flag=false;
-}
-else{
-
-    document.getElementById("yourForm").reset();
-    flag=true;
-}
-return flag;
-}
 
