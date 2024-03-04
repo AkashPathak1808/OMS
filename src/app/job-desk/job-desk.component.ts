2660572a8e1aa6b1d696ef20f0479f3bac476c77
AkashPathak1808 <akashpathak180801@gmail.com>
2024-03-04 17:11:03 +0530

    Solve Integration problem

--- src/app/job-desk/job-desk.component.ts
+++ src/app/job-desk/job-desk.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ResumeserviceService } from '../services/resumeservice.service';
 
@@ -7,11 +7,11 @@ import { ResumeserviceService } from '../services/resumeservice.service';
   templateUrl: './job-desk.component.html',
   styleUrls: ['./job-desk.component.css']
 })
-export class JobDeskComponent  {
+export class JobDeskComponent {
 
-  @ViewChild('jobApplicationModal') jobApplicationModal: any; 
+  @ViewChild('jobApplicationModal') jobApplicationModal: any;
 
-  constructor(private resumeService:ResumeserviceService, private snack: MatSnackBar) { }
+  constructor(private resumeService: ResumeserviceService, private snack: MatSnackBar) { }
 
   // constructor(private snack: MatSnackBar){}
 
@@ -24,63 +24,58 @@ export class JobDeskComponent  {
     resumeFileName: '',
   }
 
-  formSubmit(){
+  formSubmit() {
     console.log(this.resumeForm);
-    if( this.resumeForm.name != '' && this.resumeForm.email != '' && this.resumeForm.phone != '' )
-    {
+    if (this.resumeForm.name != '' && this.resumeForm.email != '' && this.resumeForm.phone != '', this.resumeForm.email != '', this.resumeForm.resumeFileName != '') {
       //backend saving code
 
-      this.snack.open("Submitted Successfully !!", '', {
+      this.resumeService.savedResume(this.resumeForm).
+        subscribe(
+          (data) => {
+            this.snack.open("Submitted Successfully !!", '', {
+              duration: 5000, verticalPosition: 'top'
+            });
+            this.resetForm();
+            this.closeModal();
+            console.log(data)
+          }, (error) => {
+            this.snack.open("Something Went Wrong !!", '', {
+              duration: 5000, verticalPosition: 'top'
+            });
+            this.closeModal();
+            console.log(error)
+          }
+        );
+    }
+    else {
+      this.snack.open("Fill form correctly !!", '', {
         duration: 5000, verticalPosition: 'top'
       });
-
-      this.resetForm();
       this.closeModal();
     }
-    else
-    {
-      this.snack.open("Something Went Wrong !!", '', {
-        duration: 5000, verticalPosition: 'top'
-      });
-
-      this.resetForm();
-      this.closeModal();
-    }
-    
-    // this.resumeService.savedResume(this.resumeForm).
-    // subscribe(
-    //   (data) => {
-        
-    //     console.log(data)
-    //   }, (error) => {
-        
-    //     console.log(error)
-    //   }
-    // );
-
   }
 
 
-    closeModal() {
-      if (this.jobApplicationModal) {
-        this.jobApplicationModal.nativeElement.click(); // Programmatically close the modal
-      }
+  closeModal() {
+    if (this.jobApplicationModal) {
+      this.jobApplicationModal.nativeElement.click(); // Programmatically close the modal
     }
+  }
 
-    private resetForm() {
-      // Reset the form by creating a new object
-      this.resumeForm = {
-        name: '',
-        phone: '',
-        email: '',
-        resumeFileName: '',
-      };
-    }
+  private resetForm() {
+    // Reset the form by creating a new object
+    this.resumeForm = {
+      name: '',
+      phone: '',
+      email: '',
+      resumeFileName: '',
+    };
+  }
 
-    resetFormOnFocusOut() {
-      // Reset the form when any input field loses focus
-      this.resetForm();
-    }
-  
-  
+  resetFormOnFocusOut() {
+    // Reset the form when any input field loses focus
+    this.resetForm();
+  }
+
+
 }
